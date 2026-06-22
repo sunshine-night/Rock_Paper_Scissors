5989e0923dbd7223a94f90478825a9c807a9dd03
MaabRizig <290771189+sunshine-night@users.noreply.github.com>
2026-06-22 15:26:23 -0400

    initialize index.html and main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+function playGame(){
+    // let computerScore = humanScore = 0;
+    // let computerChoice, humanChoice;
+    // for(let i=0;i<5;i++){
+        
+    //     playRound()
+    // }
+}
+
+function playRound(computerChoice,humanChoice){
+
+}
+
+function getComputerChoice(){
+
+}
+function getHumanChoice(){
+
+}
