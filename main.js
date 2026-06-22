8a2eb88fb7ba56c724661b9b5724e1995ec9ec1d
MaabRizig <290771189+sunshine-night@users.noreply.github.com>
2026-06-22 15:55:44 -0400

    set code for getComputerChoice()

--- main.js
+++ main.js
@@ -12,11 +12,13 @@ function playRound(computerChoice,humanChoice){
 }
 
 function getComputerChoice(){
-
+    gameChoices = ["rock","paper","scissor"];
+    choiceIndex = Math.floor(Math.random()*3);
+    return gameChoices[choiceIndex];
 }
 function getHumanChoice(){
     choice = prompt("choice either Rock, Paper, or Scissor:");
     return choice;
 }
 
-console.log(getHumanChoice());
+console.log(getComputerChoice());
